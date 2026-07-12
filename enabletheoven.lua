2e95566b436dca5bc147b9e5e6f13b701a44c0a2
Suni <jsckongju@gmail.com>
2026-07-12 13:34:50 +0700

    Update enabletheoven.lua

--- enabletheoven.lua
+++ enabletheoven.lua
@@ -1,28 +1,11 @@
 local pid = game.PlaceId
 
-local old = {
-    [77747658251236] = true,
-    [130167267952199] = true,
-    [99684056491472] = true,
-    [75159314259063] = true,
-    [123955125827131] = true,
-    [96767841099256] = true,
-    [138368689293913] = true,
-    [98826438856089] = true,
-    [75982168454032] = true,
-    [87419807194206] = true,
-    [84988808589910] = true,
-    [96105075537655] = true
-}
+local old = {77747658251236, 130167267952199, 99684056491472, 75159314259063, 123955125827131, 96767841099256, 138368689293913, 98826438856089, 75982168454032, 87419807194206, 84988808589910, 96105075537655}
+local new = {71132543521245, 91255392593879}
 
-local new = {
-    [71132543521245] = true,
-    [91255392593879] = true
-}
-
-if old[pid] then
+if table.find(old, pid) then
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/52d7bb3829a25a2f9b476d1eb84c8d1d4271fda532b61574b926dd7cd2e9d3e9/download"))()
-elseif new[pid] then
+elseif table.find(new, pid) then
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5a01e5dde490d4a90168d2a002927d987a0ba01b026a3a294b1fc55a43659c99/download"))()
 else
     print("current game is not supported")
