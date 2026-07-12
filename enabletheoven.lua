e7b4a042663c56b78b5bf75848e7260559abd9a2
Suni <jsckongju@gmail.com>
2026-07-12 14:33:18 +0700

    Update enabletheoven.lua

--- enabletheoven.lua
+++ enabletheoven.lua
@@ -4,7 +4,7 @@ local old = {77747658251236, 130167267952199, 99684056491472, 75159314259063, 12
 local new = {71132543521245, 91255392593879}
 
 if table.find(old, pid) then
-    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/52d7bb3829a25a2f9b476d1eb84c8d1d4271fda532b61574b926dd7cd2e9d3e9/download"))()
+    loadstring(game:HttpGet("https://vss.pandauth.com/kv/65f7d8f04e552714"))()
 elseif table.find(new, pid) then
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5a01e5dde490d4a90168d2a002927d987a0ba01b026a3a294b1fc55a43659c99/download"))()
 else
